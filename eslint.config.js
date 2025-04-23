c9d0a8c8221b8e4fd2c40b50d7835ec496e5faa5
Tornike Kapanadze <tornikekapan@gmail.com>
2025-04-23 09:11:24 +0300

    chore: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,41 @@
-import { eslintConfig } from 'tether-dev-docs'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPlugin from 'eslint-plugin-eslint-plugin'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginReact from 'eslint-plugin-react'
 
 export default [
-  ...eslintConfig,
   {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+      react: eslintPluginReact,
+      eslint: eslintPlugin
+    },
     rules: {
+      ...eslintConfigPrettier.rules,
+
+      // Prettier integration
       'prettier/prettier': [
         'error',
         {
-          ...eslintConfig[0].rules['prettier/prettier'][1],
+          semi: false,
+          singleQuote: true,
+          useTabs: false,
+          tabWidth: 2,
+          trailingComma: 'none',
+          bracketSpacing: true,
+          arrowParens: 'always',
+          endOfLine: 'auto',
+          embeddedLanguageFormatting: 'auto',
           plugins: ['prettier-plugin-tailwindcss']
         }
       ]
