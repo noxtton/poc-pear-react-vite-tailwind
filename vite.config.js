6e60fa791bb6f3e4e5563c136a656eff1685b1d3
Tornike Kapanadze <tornikekapan@gmail.com>
2025-09-03 01:40:42 +0300

    fix: code refactor and imrovements

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: './src/index.jsx',
+      output: {
+        entryFileNames: `main/index.js`,
+        chunkFileNames: `main/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names && assetInfo.names[0].endsWith('.css')) {
+            return 'main/index.css'
+          }
+          return 'main/[name][extname]'
+        }
+      }
+    }
+  }
 })
